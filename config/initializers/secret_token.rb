c87f4a806c6a53ed01c57e76ace4c3707064faa8
Aaron Johnson <aaronjohnsonis@gmail.com>
2013-03-20 20:40:02 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webd2::Application.config.secret_token = 'b83278d9ae012855dd887ecebc5d29f326daf5f58f31ccee6ddcf0ead54add6623275e0994b4e3e14d6a5624c44df6fd70e8b46e913bcdcf1e70a603e2562e2f'
